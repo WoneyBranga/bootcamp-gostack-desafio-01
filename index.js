b0b83f30d20b07c5e1f90a49168e25ed0075820e
woneybranga <woney.branga@gmail.com>
2019-07-03 10:57:17 -0300

    criadas rotas solicitadas

--- index.js
+++ index.js
@@ -2,8 +2,68 @@ const express = require("express");
 
 const server = express();
 
-server.get("/", (req, res) => {
-  return res.json({ message: "Hello World" });
+server.use(express.json());
+
+const projects = [];
+
+// lista projetos
+server.get("/projects", (req, res) => {
+  return res.json(projects);
+});
+
+//lista projeto especifico
+server.get("/projects/:index", (req, res) => {
+  const { index } = req.params;
+
+  return res.json(projects[index]);
+});
+
+// add projeto
+server.post("/projects/", (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(projects);
+});
+
+// delete project
+server.delete("/projects/:id", (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex(p => p.id === id);
+
+  projects.splice(projectIndex, 1);
+
+  return res.json(projects);
+});
+
+// update project
+server.put("/projects/:id", (req, res) => {
+  const { id } = req.params;
+
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id === id);
+
+  project.title = title;
+  return res.json(project);
+});
+
+server.post("/projects/:id/tasks", (req, res) => {
+  const { id } = req.params;
+  const { tasks } = req.body;
+
+  const project = projects.find(p => p.id === id);
+
+  project.tasks.push(tasks);
+  return res.json(project);
 });
 
 server.listen(3000);
